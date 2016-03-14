c031e7183bed3fbf6b5263b4a3338d31f3f9f880
exitir <exitir@gmail.com>
2016-03-14 02:18:20 -0700

    Added files via upload
    Added Map changes

--- dist/js/pages/dashboard2.js
+++ dist/js/pages/dashboard2.js
@@ -172,19 +172,19 @@ $(function () {
   $('#world-map-markers').vectorMap({
     map: 'world_mill_en',
     normalizeFunction: 'polynomial',
-    hoverOpacity: 0.7,
+    hoverOpacity: 1.0,
     hoverColor: false,
     backgroundColor: 'transparent',
     regionStyle: {
       initial: {
         fill: 'rgba(210, 214, 222, 1)',
-        "fill-opacity": 50,
+        "fill-opacity": 80,
         stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
+        "stroke-width": 3,
+        "stroke-opacity": 5
       },
       hover: {
-        "fill-opacity": 70,
+        "fill-opacity": 200,
         cursor: 'pointer'
       },
       selected: {
@@ -204,11 +204,11 @@ $(function () {
       {latLng: [43.73, 7.41], name: 'Monaco'},
       {latLng: [-0.52, 166.93], name: 'Nauru'},
       {latLng: [-8.51, 179.21], name: 'Tuvalu*/
-      {latLng: [43.93, 12.46], name: 'San Marino'},
+      {latLng: [43.93, 12.46], name: 'San Marino  PIP-62   LAN-406   WireLess-301 '},
       /*{latLng: [47.14, 9.52], name: 'Liechtenstein'},
       {latLng: [7.11, 171.06], name: 'Marshall Islands'},
       {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},*/
-      {latLng: [3.2, 73.22], name: 'Maldives'},
+      {latLng: [3.2, 73.22], name: 'Maldives   PIP-80   LAN-731   WireLess-527 '},
       /*{latLng: [35.88, 14.5], name: 'Malta'},
       {latLng: [12.05, -61.75], name: 'Grenada'},
       {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
@@ -217,7 +217,7 @@ $(function () {
       {latLng: [-4.61, 55.45], name: 'Seychelles'},
       {latLng: [7.35, 134.46], name: 'Palau'},
       {latLng: [42.5, 1.51], name: 'Andorra'},*/
-      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
+      {latLng: [14.01, -60.98], name: 'Saint Lucia   PIP-117   LAN-608   WireLess-421 '},
       /*{latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
       {latLng: [1.3, 103.8], name: 'Singapore'},
       {latLng: [1.46, 173.03], name: 'Kiribati'},
@@ -225,7 +225,7 @@ $(function () {
       {latLng: [15.3, -61.38], name: 'Dominica'},
       {latLng: [-20.2, 57.5], name: 'Mauritius'},
       {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'},*/
-      {latLng: [26.02, 50.55], name: 'Bahrain'}
+      {latLng: [26.02, 50.55], name: 'Bahrain   PIP-141   LAN-514  WireLess-461 '}
     ]   
 		
   });
